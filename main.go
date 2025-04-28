4513fcae83ccd275ae205b1ef4f0a8cf52df7fff
rpowelson12 <randy.powelson@yahoo.com>
2025-04-27 23:25:05 -0700

    Commands baseline

--- main.go
+++ main.go
@@ -1,22 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		
-		scanner.Scan()
-		text := scanner.Text()
-		
-		clean := cleanInput(text)
-		
-		fmt.Println("Your command was:", clean[0])
-	}
+	startRepl()
 }
