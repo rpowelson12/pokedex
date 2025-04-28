010bec4fb2802808a6032b479830d1adc13c0a9e
rpowelson12 <randy.powelson@yahoo.com>
2025-04-27 22:48:03 -0700

    Initial REPL

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
 
+	for {
+		fmt.Print("Pokedex > ")
+		
+		scanner.Scan()
+		text := scanner.Text()
+		
+		clean := cleanInput(text)
+		
+		fmt.Println("Your command was:", clean[0])
+	}
 }
